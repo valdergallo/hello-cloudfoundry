9777742f6f3cc370ed5190e4ebf102b5ff03ef3d
Valder Gallo <valder.gallo@nkey.com>
2018-05-17 17:17:08 -0300

    using SAP PORT environment

--- hello.py
+++ hello.py
@@ -1,13 +1,16 @@
 import bottle
+import os
+
 
 # uwsgi will call application
 app = application = bottle.Bottle()
+PORT = os.environ.get("PORT", 8080)
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return "Hello World! SAP CF"
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=PORT, debug=True)
